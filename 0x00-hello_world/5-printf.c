d1a53650d4562d195986eafe66971848d1963647
victorkimutai <victorkimutai374@gmail.com>
2023-06-15 16:31:36 -0700

    using printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -printing output using printf function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
