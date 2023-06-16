2955afeb90a8cf8052ad3adbee15c9911109002d
victorkimutai <victorkimutai374@gmail.com>
2023-06-15 17:15:21 -0700

    using sizeof function

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -using sizeof function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int i;
+char z;
+long int k;
+long long int m;
+float f;
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(z));
+printf("size of a int: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(k));
+printf("size of a long long: %lu byte(s)\n", (unsigned long long)sizeof(m));
+printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+return (0);
+}
