a169eebef7477badaaebcc5b2c27cbf62f2430a0
victorkimutai <victorkimutai374@gmail.com>
2023-06-15 17:17:45 -0700

    using sizeof function

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ float f;
 printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(z));
 printf("size of a int: %lu byte(s)\n", (unsigned long)sizeof(i));
 printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(k));
-printf("size of a long long: %lu byte(s)\n", (unsigned long long)sizeof(m));
+printf("size of a long long int%s/[\s\t]*$//: %lu byte(s)\n", (unsigned long long)sizeof(m));
 printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
