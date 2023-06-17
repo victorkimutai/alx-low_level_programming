a4fa83b6e3f9ca5913b340845c3649f62c9f0ab2
victorkimutai <victorkimutai374@gmail.com>
2023-06-17 14:21:12 -0700

    using while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char m;
+
+	m = 'a';
+	while
+		(m <= 'z')
+	{
+		putchar(m);
+		m++;
+	}
+	putchar('\n');
+	return (0);
+}
