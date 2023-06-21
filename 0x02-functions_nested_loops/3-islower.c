587621a7bea632f4c9f1d94b8f9c8c4a15063b16
victorkimutai <victorkimutai374@gmail.com>
2023-06-21 07:42:05 -0700

    cheking if the letters is lowercase and return 1 using ASCII

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower -check for lower case characters
+ * Return: 1 for lowercase character or 0 for anything else
+ */
+
+int _islower(int c)
+
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+return (0);
+}
