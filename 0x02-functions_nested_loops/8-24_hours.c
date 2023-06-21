0cb450ae26322e3509af3426dc502fe2f9fe4db8
victorkimutai <victorkimutai374@gmail.com>
2023-06-21 14:56:46 -0700

    printing hours of work

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
  /**
-  * jack_bauer - Entry point
-  * Retutn: ...
+  * jack_bauer -Print every minutes of the day of jack bauer
+  * Return: ...
   */
 
 void jack_bauer(void)
@@ -13,7 +13,7 @@ for (a = 0; a <= 2; a++)
 {
 for (b = 0; b <= 9; b++)
 {
-if ((a <=1 && b <= 9) || (a <= 2 && b <= 3))
+if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 {
 for (c = 0; c <= 5; c++)
 {
