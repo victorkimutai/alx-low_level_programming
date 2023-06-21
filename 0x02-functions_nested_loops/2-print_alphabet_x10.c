01dee5fefd517560389488f4470240349c4a7990
victorkimutai <victorkimutai374@gmail.com>
2023-06-21 05:55:07 -0700

    using for to print alphabet from a-z 20 times with while loop

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,9 +7,13 @@ void print_alphabet_x10(void)
 {
 int k;
 
-for (k = 'a'; k <= 'z'; k++)
-while (k <= 10)
+int i = 0;
+
+while (k < 10)
+for (k = 'a'; k<= 'z'; k++)
 _putchar(k);
-k++;
+{
 _putchar('\n');
+i++;
+}
 }
