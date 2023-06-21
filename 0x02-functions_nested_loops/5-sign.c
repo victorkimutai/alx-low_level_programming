187521e52a928ea300a884f4a82d2cd6cb1d078d
victorkimutai <victorkimutai374@gmail.com>
2023-06-21 09:45:00 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
 #include "main.h"
  /**
-  * print_sign - Prints the sign of a number
+  * print_sign - The function prints the sign of a number
   * @n: The number to be checked
   * Return: 1 for positive num, -1 for negative num
   anything else
