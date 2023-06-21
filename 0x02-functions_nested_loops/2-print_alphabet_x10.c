6d0a84e9bccb2ddde1d76442eeef3aff7861a3b9
victorkimutai <victorkimutai374@gmail.com>
2023-06-21 07:21:33 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,15 +8,15 @@ void print_alphabet_x10(void)
 int k;
 
 int i = 0;
-{
 while (k < 10)
 {
-for (k = 'a'; k<= 'z'; k++)
-_putchar(k);
+for (k = 'a'; k <= 'z'; k++)
 {
+_putchar(k);
+}
 _putchar('\n');
 i++;
 }
 }
-}
-}
+
+
