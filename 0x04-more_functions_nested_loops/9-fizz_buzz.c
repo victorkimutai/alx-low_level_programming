c23ab292bdd4c3fdbfa2c1eb4bc46976e560df83
victorkimutai <victorkimutai374@gmail.com>
2023-06-23 03:12:18 -0700

    adding \n to add a new line

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,21 +9,27 @@ int main(void)
 {
 int i;
 for (i = 1; i <= 100; i++)
+{
 if (i % 5 == 0 && i % 3 ==  0)
 {
-printf("FizzBuzz ");
+printf("FizzBuzz");
 }
 else if (i % 3 ==  0)
 {
-printf("Fizz ");
+printf("Fizz");
 }
 else if (i % 5 == 0)
 {
-printf("Buzz ");
+printf("Buzz");
 }
 else
 {
 printf("%d ", i);
+}
+if (i < 100)
+{
+printf(" ");
+}
 }
 printf("\n");
 return (0);
