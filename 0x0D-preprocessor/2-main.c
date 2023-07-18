28c22679dae9bd631d140cd1ae68d30846cb7305
victorkimutai <victorkimutai374@gmail.com>
2023-07-18 15:04:04 -0700

    c preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * ain - gives the name of the file
+ * Return: Always 0 (Success)
+ */
+int ain(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+#0x0D. c_preprocessor
