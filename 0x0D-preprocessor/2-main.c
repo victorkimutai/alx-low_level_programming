33a4fd093a9025ab95332a1741cca57389cf3057
victorkimutai <victorkimutai374@gmail.com>
2023-07-18 15:12:37 -0700

    inserting function main

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * ain - gives the name of the file
+ * main - gives the name of the file
  * Return: Always 0 (Success)
  */
-int ain(void)
+int main(void)
 {
 printf("%s\n", __FILE__);
 return (0);
